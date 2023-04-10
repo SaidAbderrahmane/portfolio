e0ce9f2dfbd9b75c9d925d681475d45e1344e475
saidabder <saidabd3@gmail.com>
2023-04-10 13:19:27 +0100

    card changes

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,10 +8,10 @@ import Typography from '@mui/material/Typography';
 
 export default function MediaCard() {
   return (
-    <Card sx={{ maxWidth: 345 }}>
+    <Card sx={{ marginLeft:5, margin: 2, width: 450, height:450 }}>
       <CardMedia
-        sx={{ height: 140 }}
-        image="/static/images/cards/contemplative-reptile.jpg"
+        sx={{ height: 300 }}
+        image="https://www.zdnet.com/a/img/resize/adcb2eb2cc3f5562f7e80931308e2a08b01947ce/2019/08/19/4a663776-f4a9-4f89-9bee-2d07ee052f5b/istock-1147195672-11.jpg?auto=webp&fit=crop&height=900&width=1200"
         title="green iguana"
       />
       <CardContent>
@@ -23,7 +23,7 @@ export default function MediaCard() {
         </Typography>
       </CardContent>
       <CardActions>
-        <Button size="small">Source</Button>
+        <Button href='https://www.github.com/saidabderrahmane' size="small">Source</Button>
         <Button size="small">Demo</Button>
       </CardActions>
     </Card>
